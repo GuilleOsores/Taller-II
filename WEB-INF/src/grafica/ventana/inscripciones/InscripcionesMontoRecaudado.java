4f55afccb7f9c9a8a25adc3f49c3cb70d240ac31
gnovasco <gatutus@gmail.com>
2018-03-11 16:44:46 -0300

    DISPOSE_ON_CLOSE

--- WEB-INF/src/grafica/ventana/inscripciones/InscripcionesMontoRecaudado.java
+++ WEB-INF/src/grafica/ventana/inscripciones/InscripcionesMontoRecaudado.java
@@ -50,7 +50,7 @@ public class InscripcionesMontoRecaudado extends Ventana {
 		
 		setTitle("Monto recaudado por Inscripciones");
 		setResizable(false);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 390, 175);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
