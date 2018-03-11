395f665035dc8fc33b329b01d880394d284c8e19
pelusa <guille.osores@gmail.com>
2018-03-11 17:35:10 -0300

    para web, falta terminar

--- Escolaridad.jsp
+++ Escolaridad.jsp
@@ -1,44 +1,42 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
-
-<!--<jsp:useBean id='datosPer' scope='session' class='valueObjects.DataPersona' />-->  
+<%@ page import="java.util.LinkedList" %>
+<jsp:useBean id='escolaridad' scope='request' class='java.util.LinkedList' />
 
 <html>
-  <head>
-    <title> Escolaridad </title>
-  </head>
+<head>
+<title>Escolaridad</title>
+</head>
 
-  <body bgcolor="#DCFBD2">
-	<table border="5" width="50%" bordercolor="#00AE00" height="50" bgcolor="#B6F4AA">
-		<tr>
-			<td>
-				<p align="center"><b>
-				<font face="Tahoma" size="4"> Bienvenido ${datosPer.nombre} de ${datosPer.procedencia} </font></b>
-			</td>
-		</tr>
+<body>
+	<form action='Escolaridad' method='POST'>
+		<table border="5" width="50%" height="50">
+			<tr>
+				<td><span>Cédula</span></td>
+				<td><input type="text" id="cedula" name="cedula"/></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td><select id="tipo" name="tipo">
+						<option value="true" selected="true">Completo</option>
+						<option value="false">Intermedio</option>
+				</select></td>
+			</tr>
+		</table>
+		<p>
+			<input type='submit' value='ENVIAR'>
+		</p>
+	</form>
+	<table border="5" width="50%" height="50">
+		<c:forEach items="${escolaridad}" var="i" >
+			<tr>
+				<td> ${i.getNumero()} </td>
+				<td> ${i.getAnioLectivo()} </td>
+				<td> ${i.getMontoBase()} </td>
+				<td> ${i.getCalificacion()} </td>
+				<td> ${i.getAsignaturaNombre()} </td>
+			</tr>
+		</c:forEach>
 	</table>
-
-    <form action='Escolaridad' method='POST'>
-      <table border="5" width="50%" bordercolor="#00AE00" height="50" bgcolor="#B6F4AA">
-      <tr>
-			<td>
-				<span>Cédula</span>
-			</td>
-			<td>
-				<input type="text" id="cedula" />
-			</td>
-		</tr>
-		<tr>
-			<td>
-			</td>
-			<td>
-				<select name="completo">
-					<option value="true"> Completo </option>
-					<option value="false"> Intermedio </option>
-				</select>
-			</td>
-		</tr>
-	</table>
-      <p> <input type='submit' value='ENVIAR'> </p>
-    </form>
-  </body>
+	
+</body>
 </html>
--- WEB-INF/src/servlets/Escolaridad.java
+++ WEB-INF/src/servlets/Escolaridad.java
@@ -1,6 +1,8 @@
 package servlets;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -10,66 +12,70 @@ import javax.servlet.http.HttpSession;
 
 import exception.AlumnoNoExisteException;
 import logica.IFachada;
+import logica.vo.VOEscolaridad;
 
 
 public class Escolaridad extends Servlet {
 	private static final long serialVersionUID = 1L;
-	
+
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) {
-		
+
 	}
 
 	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-	    // obtengo los datos del Request
-	    String cedula = req.getParameter("cedula").trim();
-	    String completo = req.getParameter("completo").trim();
-	    boolean error = true;
-	    	    
-	    String msgError = new String();
-	    if( (cedula == null) || cedula.equals("") ) {
-			msgError = "La cedula no puede estar vacio";
+		// obtengo los datos del Request
+		String cedula = req.getParameter("cedula");
+		String completo = req.getParameter("tipo");
+		boolean error = true;
+
+		String msgError = new String();
+		if( (cedula == null) || cedula.trim().equals("") ) {
+			msgError = "La cedula no puede estar vacio" + cedula;
 			req.setAttribute("msgError", msgError);
-		
-	    } else if ( !soloNumeros( cedula ) ) {
-	    	msgError = "La cédula debe ser numérica";
-	    	req.setAttribute("msgError", msgError);
-	    	
-	    } else if ( completo != "true" || completo != "false" ) {
-	    	msgError = "Opción inválida";
-	    	req.setAttribute("msgError", msgError);
-	    	
+
+		} else if ( !soloNumeros( cedula ) ) {
+			msgError = "La cédula debe ser numérica";
+			req.setAttribute("msgError", msgError);
+
+		} else if ( !( completo.equals( "true" ) || completo.equals( "false" ) ) ) {
+			msgError = "Opción inválida: " + completo;
+			req.setAttribute("msgError", msgError);
+
 		} else {		    
 			IFachada iFachada = conectar( req );
-			
+
 			if( iFachada != null ) {
 				int intCedula	= Integer.parseInt( cedula );
 				boolean parcial = !Boolean.parseBoolean( completo );
-				
+
 				try {
-					iFachada.escolaridad( intCedula, parcial );
+					List<VOEscolaridad> lvoe = iFachada.escolaridad( intCedula, parcial );
+					req.setAttribute( "escolaridad", lvoe );
+
+					error = false;
+
 				} catch (AlumnoNoExisteException e) {
 					msgError = "El alumno no existe";
-			    	req.setAttribute("msgError", msgError);
+					req.setAttribute("msgError", msgError);
 				}
-				
+
 			}
-			
+
 			HttpSession session = req.getSession();
-		    //DataPersona datosPer = new DataPersona(nombre,procedencia);
-		    synchronized (session)
-		    {
-		        session.setAttribute("cedula", cedula);    
-		    }	    
-	    }    
-	    
+			synchronized (session)
+			{
+				session.setAttribute("cedula", cedula);    
+			}	    
+		}    
+
 		// forwardeo a la página apropiada
 		RequestDispatcher rd;
 		if (!error)
-			rd = req.getRequestDispatcher("Mensaje.jsp");
+			rd = req.getRequestDispatcher("Escolaridad.jsp");
 		else {
 			rd = req.getRequestDispatcher("Error.jsp");
 		}
-			
+
 		rd.forward(req, resp);
 	}
 }
